3d9082ce093d5b13dc5b349a831e7e887f63defd
macbookpro <bratukhinmaks@gmail.com>
2022-02-23 14:55:33 +0100

    initial commit

---
+++ apps/potronus-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'potronus-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'potronus-app';
+}
